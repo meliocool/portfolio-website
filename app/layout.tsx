f473608388308abdcbb2517beb6e4fe459b3bc9a
melio <dhitanimam05@gmail.com>
2025-09-18 18:21:10 +0700

    fix: metadata stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,30 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Dhitan",
+  title: "Dhitan - Portfolio",
   description: "Dhitan's Portfolio Website",
-  icons: "/okarun.png",
+  openGraph: {
+    title: "Dhitan – Portfolio",
+    description: "Dhitan's Portfolio Website",
+    url: "https://dhitan.dev",
+    siteName: "Dhitan Portfolio",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Dhitan's Portfolio Website",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Dhitan – Portfolio",
+    description: "Dhitan's Portfolio Website",
+    images: ["/og-image.png"],
+  },
 };
 
 export default function RootLayout({
